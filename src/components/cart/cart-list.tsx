079b181c9bdda9ed5869e924565a606cd1a9bf0f
Aakash-Yadav23 <aakashdev24@gmail.com>
2024-07-10 13:00:07 +0530

    refactor: added components ,removed prettier

---
+++ src/components/cart/cart-list.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const CartList = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil est
+      veritatis magni similique nesciunt odio numquam maxime facilis nemo
+      inventore unde, ad quasi voluptatum maiores corporis voluptatibus iste?
+      Suscipit, veniam voluptas ad explicabo optio odit culpa omnis voluptatem
+      quas alias ratione molestias quam? Repellat illum voluptate nostrum, qui
+      rem vitae maiores natus veniam repudiandae nobis ab, quidem corporis,
+      porro itaque delectus aperiam sunt libero reprehenderit. Fugit tempora
+      repellendus qui quasi expedita dolor vitae cum assumenda doloremque velit!
+      Doloremque ut illo dolore et veritatis quisquam placeat quos aliquam
+      explicabo ipsum est quibusdam delectus culpa ipsa quia, tenetur magnam
+      doloribus ad quasi.
+    </div>
+  );
+};
+
+export default CartList;
