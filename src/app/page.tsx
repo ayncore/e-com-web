753477aba2f9702f89b08b8f62cbfe8e11a45eff
GODCarNageOP <yashvharale@gmail.com>
2024-07-11 12:27:47 +0530

    refactor(package.json): heroicons & framer-motion initialized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-
+import Hero from "@/components/home/hero";
 
 export default function Home() {
   return (
-    <div className="bg-white h-screen w-full">
-     
-    </div>
+   <Hero/>
   );
 }
---
+++ src/components/home/hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="hero bg-white h-screen w-full"></div>
+  )
+}
+
+export default Hero
