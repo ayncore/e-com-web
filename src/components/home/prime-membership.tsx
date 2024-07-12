06ea940d0b08ab63b88c72bc83e530daef475e30
GODCarNageOP <yashvharale@gmail.com>
2024-07-12 14:43:33 +0530

    feat(home/discover.tsx): functionalities added

--- src/components/home/prime-membership.tsx
+++ src/components/home/prime-membership.tsx
@@ -6,7 +6,7 @@ import { IoMdArrowRoundForward } from "react-icons/io";
 const PrimeMembership = () => {
   return (
     <div className="primemembership h-full w-full bg-white rounded-xl my-10 flex justify-between flex-col lg:flex-row gap-20 lg:gap-10 py-10">
-      <div className="left w-[40%] p-2 flex items-center flex-col justify-center">
+      <div className="left w-full lg:w-[40%] p-2 flex items-center flex-col justify-center">
         <h1 className="text-black font-semibold text-5xl lg:text-6xl max-w-lg">
           Earn free money with AYNcore
         </h1>
@@ -18,7 +18,7 @@ const PrimeMembership = () => {
             <button className="bg-white text-black rounded-full px-4 py-3 lg:px-6 lg:py-4 hover:bg-slate-100 border-slate-200 border flex items-center gap-2 lg:gap-5 text-md font-semibold">Discover More <IoMdArrowRoundForward className="w-6 h-6"/></button>
         </div>
       </div>
-      <div className="right w-[60%]">
+      <div className="right w-full lg:w-[60%]">
         <Image src={money} alt="" className="p-5" />
       </div>
     </div>
